159b9bcd46e7c22198f5598195141cbb7f33c358
Tanya Libatter <tanyad@venn.city>
2023-02-05 08:53:09 +0200

    chore: some improvments

--- apps/items/src/components/itemsSkeleton.tsx
+++ apps/items/src/components/itemsSkeleton.tsx
@@ -7,9 +7,9 @@ export const ItemsSkeleton = () => {
   return (
     <Box sx={{ width: "100%" }}>
       <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        {Array.from(new Array(6)).map(item => (
-          <Grid xs={4}>
-            <Skeleton variant="rectangular" width={380} height={275} />
+        {Array.from(new Array(9)).map((item,index) => (
+          <Grid xs={4} key={index}>
+            <Skeleton  variant="rectangular" width={380} height={275} />
           </Grid>
         ))}
       </Grid>
